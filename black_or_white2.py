282366846abc32dfd73cb9b6eb4ddf4992f7ed63
Limex1554 <limex1554@wp.pl>
2024-10-10 09:32:17 +0200

    Update black_or_white2.py
    Signed-off-by: Limex1554 <limex1554@wp.pl>

--- black_or_white2.py
+++ black_or_white2.py
@@ -1,15 +1,15 @@
-litery = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8}
-cyfry = (1, 2, 3, 4, 5, 6, 7, 8)
+letters = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8}
+numbers = (1, 2, 3, 4, 5, 6, 7, 8)
 
-pole = input("Please enter the name of your chess field: ")
+chessfield = input("Please enter the name of your chess field: ")
 
-if pole[0] in litery and int(pole[1]) in cyfry:
-    pole0 = litery.get(pole[0])
-    pole2 = pole0 +int(pole[1])
+if chessfield[0] in letters and int(chessfield[1]) in numbers:
+    chessfield0 = letters.get(chessfield[0])
+    chessfield2 = chessfield0 +int(chessfield[1])
     print("Such chess field exists")
-    if pole2 % 2 == 0:
+    if chessfield2 % 2 == 0:
         print("Your chess field is black")
-    elif pole2 % 2 == 1:
+    elif chessfield2 % 2 == 1:
         print("Your chess field is white")
 else:
     print("There is no such chess field")
