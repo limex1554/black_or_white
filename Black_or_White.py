6f47ba743cad77ae10da0a8384cf99f2916698e6
Limex1554 <limex1554@wp.pl>
2024-10-11 08:34:55 +0200

    Update Black_or_White.py
    Signed-off-by: Limex1554 <limex1554@wp.pl>

--- Black_or_White.py
+++ Black_or_White.py
@@ -1,15 +1,22 @@
 letters = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8}
 numbers = (1, 2, 3, 4, 5, 6, 7, 8)
 
-chessfield = input("Please enter the name of your chess field: ")
+while True:
+    chessfield = input("Please enter the name of your chess field: ")
+    if chessfield:
+        break
+    print("Input cannot be empty. Please try again.")
 
-if chessfield[0] in letters and int(chessfield[1]) in numbers:
-    chessfield0 = letters.get(chessfield[0])
-    chessfield2 = chessfield0 +int(chessfield[1])
-    print("Such chess field exists")
-    if chessfield2 % 2 == 0:
-        print("Your chess field is black")
-    elif chessfield2 % 2 == 1:
-        print("Your chess field is white")
-else:
-    print("There is no such chess field")
+try:
+    if chessfield[0] in letters and int(chessfield[1]) in numbers:
+        chessfield0 = letters.get(chessfield[0])
+        chessfield2 = chessfield0 +int(chessfield[1])
+        print("Such chess field exists")
+        if chessfield2 % 2 == 0:
+            print("Your chess field is black")
+        elif chessfield2 % 2 == 1:
+            print("Your chess field is white")
+    else:
+        print("There is no such chess field")
+except IndexError:
+    print("Index out of range")
